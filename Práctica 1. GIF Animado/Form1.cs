c5993f6496cc332533d4219ec0156ca86827d3ea
BandaJuarezYocelinYamileth <yoce.banda07@gmail.com>
2025-10-14 18:58:30 -0600

    Práctica 1

---
+++ Práctica 1. GIF Animado/Form1.cs
@@ -0,0 +1,15 @@
+namespace Práctica_1._GIF_Animado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
