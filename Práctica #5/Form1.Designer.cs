0df0cf97e290b2e2f9dd7fc71bb6e7a4f71ccb4f
BandaJuarezYocelinYamileth <yoce.banda07@gmail.com>
2025-10-16 09:39:10 -0600

    Práctica#5

---
+++ Práctica #5/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Práctica__5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(125, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(392, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Introduce las Dimensiones del Clindro";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(125, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Radio";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label3.Location = new Point(125, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Altura";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("BankGothic Md BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(125, 249);
+            label4.Name = "label4";
+            label4.Size = new Size(102, 16);
+            label4.TabIndex = 3;
+            label4.Text = "Base área";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("BankGothic Md BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(125, 296);
+            label5.Name = "label5";
+            label5.Size = new Size(130, 16);
+            label5.TabIndex = 4;
+            label5.Text = "Lateral área";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("BankGothic Md BT", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(125, 340);
+            label6.Name = "label6";
+            label6.Size = new Size(94, 14);
+            label6.TabIndex = 5;
+            label6.Text = "Total área";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("BankGothic Md BT", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(125, 394);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 16);
+            label7.TabIndex = 6;
+            label7.Text = "Volumen";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("BankGothic Md BT", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(116, 175);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 7;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("BankGothic Md BT", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(229, 175);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 8;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("BankGothic Md BT", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(352, 175);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 9;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(204, 58);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(204, 106);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(261, 246);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(261, 296);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(261, 340);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(261, 391);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Cilindro;
+            pictureBox1.Location = new Point(529, 48);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(209, 204);
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private PictureBox pictureBox1;
+    }
+}
